66966137ef2abf81f7ae6eb50f1d7cc251a6d9bf
Virtapatil2000 <virtapatil01@gmail.com>
2023-01-12 10:17:14 +0530

    [Virta] Adding Addition Functionality

--- CSharpBasicProgram/Program.cs
+++ CSharpBasicProgram/Program.cs
@@ -1,2 +1,13 @@
 ﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, good morning ");
+using CSharpBasicProgram;
+{
+    Console.WriteLine("Welcome to CSharp basic programs");
+    Console.WriteLine("Please enter first number");
+    int first = Convert.ToInt32(Console.ReadLine());
+    Console.WriteLine("Please enter second number");
+    int second  = Convert.ToInt32(Console.ReadLine());
+
+    Calculator calculator = new Calculator(first, second);
+    calculator.Addition();
+    Console.ReadLine();
+}
