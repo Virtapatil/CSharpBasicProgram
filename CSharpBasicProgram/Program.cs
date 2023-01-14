7dce82f8f964495399fbb92ec188c93912641b2b
Virtapatil2000 <virtapatil01@gmail.com>
2023-01-14 09:07:46 +0530

    Adding Weedays

--- CSharpBasicProgram/Program.cs
+++ CSharpBasicProgram/Program.cs
@@ -1,2 +1,8 @@
 ﻿// See https://aka.ms/new-console-template for more information
+using CSharpBasicProgram;
+using System.ComponentModel;
+
 Console.WriteLine("Hello, good morning ");
+Console.WriteLine("Enter day number");
+int day = Convert.ToInt32(Console.ReadLine());
+Weekday.FindWeekDay(day);
